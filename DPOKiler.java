b50f00122e1241b8dd8f143b6efc31dd1101e53b
Bianskie <fayalahw@upi.edu>
2024-06-04 14:10:40 +0700

    Update DPOKiler.java

--- DPOKiler.java
+++ DPOKiler.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class DPOKillerList {
+
+    //Metode utility untuk menggunakan huruf kapital pada setiap nama                   private static String capitalizeFirstLetter(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String[]> incidents = new ArrayList<>();
